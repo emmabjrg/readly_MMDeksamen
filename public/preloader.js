5a26e3d9db9c2d922caa22990089b590f6ca360e
Emma <emmabs99@gmail.com>
2024-12-15 01:18:43 +0100

    preloader sat ind mm

---
+++ public/preloader.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+
+  setTimeout(() => {
+    preloader.style.transition = "opacity 1s ease"; 
+    preloader.style.opacity = "0"; 
+
+    setTimeout(() => {
+      preloader.style.display = "none";
+    }, 1000); 
+  }, 2100); 
+});
